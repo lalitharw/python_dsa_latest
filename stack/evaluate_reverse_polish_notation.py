85a17ae726faaa35d5acc10cb67cfe99b1f93631
lalitharw <lalitharwate90@gmail.com>
2025-01-23 23:57:42 +0530

    updated

---
+++ stack/evaluate_reverse_polish_notation.py
@@ -0,0 +1,14 @@
+class ReversePolish:
+    def __init__(self,tokens) -> int:
+        self.tokens = tokens
+
+
+    # def evaluation(num1,num2,operator):
+    #     hashMap = {
+    #         "+" : +,
+    #         "-" : -,
+    #         "*" : *
+    #     }
+
+    #     get_operator = hashMap['operator']
+    #     return num1 get_operator num2
