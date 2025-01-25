8307f3825263ff6549710d553bb1e36dafd15c2b
lalitharw <lalitharwate90@gmail.com>
2025-01-25 17:14:28 +0530

    updated

---
+++ stack/simplifyPath.py
@@ -0,0 +1,11 @@
+class SimplifyPath:
+    
+
+    def simplify(self,path:str) -> str:
+        return "Helloo worf"
+    
+
+
+path = SimplifyPath()
+
+# print(path.simplify("/home/user/Documents/../Pictures"))
