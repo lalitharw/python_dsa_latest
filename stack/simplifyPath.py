ab0980f8b2e65ae625425b378090c5907919764a
lalitharw <lalitharwate90@gmail.com>
2025-01-25 18:40:34 +0530

    updated

--- stack/simplifyPath.py
+++ stack/simplifyPath.py
@@ -1,11 +1,29 @@
 class SimplifyPath:
-    
 
-    def simplify(self,path:str) -> str:
-        return "Helloo worf"
-    
+    def simplify(self,path:str) -> list:
+
+        path_array = path.split("/")
+        stk = []
+        for p in path_array:
+            if p == "..":
+                if stk:
+                    stk.pop()
+            elif p == "." or p == "":
+                continue
+            
+            else:
+                stk.append(p)
+
+        
+
+        return "/" + "/".join(stk) if stk else "/"
 
 
 path = SimplifyPath()
 
-# print(path.simplify("/home/user/Documents/../Pictures"))
+
+
+# If the delimiter is at the start or end of the string, or appears consecutively, you may get empty strings as part of the result:
+print(path.simplify("/home//foo/"))
+print(path.simplify("/../"))
+print(path.simplify("/.../"))
