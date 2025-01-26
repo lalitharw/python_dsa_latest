7ca97aaca5f7b4f55033c257d0cbc1d80755066d
lalitharw <lalitharwate90@gmail.com>
2025-01-26 15:42:30 +0530

    updated

--- stack/removeKDigit.py
+++ stack/removeKDigit.py
@@ -1,7 +1,44 @@
 class RemoveKDigit:
-    def __init__(self):
-        return "Hello world"
+    # https://algo.monster/liteproblems/402
+
+    '''
+    basically all we have to do it
+    first we checked if stack is empty
+    if it is empty append in stack
+    if not check if top most element in stack[-1]  is greater than the current element
+    if yes pop() the element from stack and append the current element smaller element in it.
+    '''
+
+    def __init__(self,arr,digit):
+        self.digit = digit
+        self.arr = arr
+
+    def removeDigits(self):
+        stk = []
+
+        digitsRemoved = 0
+
+        for a in self.arr:
+            if(digitsRemoved > self.digit):
+                break
+
+            if(stk):
+                if(stk[-1] > a):
+                    digitsRemoved += 1
+                    stk.pop()
+                    stk.append(a)
+                else:
+                    stk.append(a)
+            else:
+                stk.append(a)
+
+        return ("".join(stk).lstrip("0"))
+
+
+
     
 
 
-k = RemoveKDigit()
+k = RemoveKDigit("1432219",3)
+
+print(k.removeDigits())
