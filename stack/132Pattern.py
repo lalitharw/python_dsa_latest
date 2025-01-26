b8982c905851ba30bec11078f79a98c87c0deadd
lalitharw <lalitharwate90@gmail.com>
2025-01-26 20:34:29 +0530

    updated

--- stack/132Pattern.py
+++ stack/132Pattern.py
@@ -1,8 +1,20 @@
 class PatternOneThreeTwo:
 
-    def __init__(self):
-        return None
+    def __init__(self,arr):
+        self.arr = arr
     
+    def pattern(self):
+
+        stk = []
+
+        for a in self.arr:
+            i = 0
+            # if stk:
+            #     if(i == )
+
+            # else:
+            #     stk.append(a)
+            #     i += 1
 
 pattern = PatternOneThreeTwo()
 
